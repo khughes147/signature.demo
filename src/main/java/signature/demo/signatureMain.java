ed83eee855fb5dda0b487f064f24e617f67f2ee0
khugh <khughes147@gmail.com>
2018-07-02 13:45:41 +0100

    Working version. Next up is producer GUI

--- src/main/java/signature/demo/signatureMain.java
+++ src/main/java/signature/demo/signatureMain.java
@@ -18,7 +18,7 @@ public class signatureMain {
     public static void main(String[] args) throws Exception {
 
         Web3j web3j = Web3j.build(new HttpService());
-        Credentials credentials = WalletUtils.loadCredentials("Ethereum", "C:\\Users\\kieran\\Documents\\EthereumProjectChain\\data\\keystore/UTC--2018-06-20T16-20-55.193747000Z--83e08a5e77901126a16721590a8e1379fb7b1b6b");
+        Credentials credentials = WalletUtils.loadCredentials("Ethereum", "C:\\Users\\khugh\\Documents\\EthereumProjectChain\\data\\keystore/UTC--2018-06-22T09-50-52.016355100Z--fa5629c569f658a445cd7c6e7714bb715cee6a8b");
         byte[] HexMessage = Hash.sha3("TEST".getBytes());
         System.out.println("Address: " + credentials.getAddress());
         System.out.println("");
@@ -34,18 +34,18 @@ public class signatureMain {
 
        // System.out.println(VerificationSmartContract.deploy(web3j,credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send().getContractAddress());
        // System.out.println(AddressStorage.deploy(web3j,credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send().getContractAddress());
-      //  System.out.println(VerifiedData.deploy(web3j,credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send().getContractAddress());
+     //   System.out.println(VerifiedData.deploy(web3j,credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT).send().getContractAddress());
 
-   //  AddressStorage aStore = AddressStorage.load("0x40e2a027c2aa8b9e934d69e5f32325ff04618ec9", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
-      //  aStore.addAddress("0x83e08a5e77901126a16721590a8e1379fb7b1b6b").send();
-     //   System.out.println(aStore.getAddress("0x83e08a5e77901126a16721590a8e1379fb7b1b6b").send());
+    // AddressStorage aStore = AddressStorage.load("0x06a5a9d42f8409ad279c1dba4763476147d99443", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
+     //   aStore.addAddress("0xfa5629c569f658a445cd7c6e7714bb715cee6a8b").send();
+     //  System.out.println(aStore.getAddress("0xfa5629c569f658a445cd7c6e7714bb715cee6a8b").send());
 
-     // VerificationSmartContract aVerifier = VerificationSmartContract.load("0x80530de5a509389a77dd418413c48ea434651b3b", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
-      //aVerifier.verify("Kieran", "Hughes", HexMessage, BigInteger.valueOf(27), signature.getR() , signature.getS()).send();
-      //  aVerifier.setVerifiedDataAddress("0x4087e6c5b789399c10216c9778660dba94dced3f").send();
+   //  VerificationSmartContract aVerifier = VerificationSmartContract.load("0xf2527b7254a875a2b4b95d9c9e9811e5d3b72731", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
+    // aVerifier.verify("Jordanstown", "Wang", HexMessage, BigInteger.valueOf(28), signature.getR() , signature.getS()).send();
+      //  aVerifier.setVerifiedDataAddress("0x7beb1fc32f8394da52cd9d98abfc521f6b432027").send();
 
-     VerifiedData aData = VerifiedData.load("0x4087e6c5b789399c10216c9778660dba94dced3f", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
-       System.out.println(aData.outputAll(BigInteger.valueOf(0)).send());
+    VerifiedData aData = VerifiedData.load("0x7beb1fc32f8394da52cd9d98abfc521f6b432027", web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT);
+    System.out.println(aData.outputAll(BigInteger.valueOf(2)).send());
      //   System.out.println(aData.searchRecords("Kieran").send());
     } 
 }
